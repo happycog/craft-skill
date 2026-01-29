1cb914649414b260b8ff45a2cc2b6d567dd70712
markhuot <mark@markhuot.com>
2026-01-28 20:40:32 -0500

    Refactor FieldLayoutsController to use spread operator for tool calls

--- src/controllers/FieldLayoutsController.php
+++ src/controllers/FieldLayoutsController.php
@@ -16,42 +16,42 @@ class FieldLayoutsController extends Controller
     public function actionCreate(): Response
     {
         $tool = \Craft::$container->get(CreateFieldLayout::class);
-        return $this->callTool($tool);
+        return $this->callTool($tool(...));
     }
 
     public function actionGet(): Response
     {
         $tool = \Craft::$container->get(GetFieldLayout::class);
-        return $this->callTool($tool, useQueryParams: true);
+        return $this->callTool($tool(...), useQueryParams: true);
     }
 
     public function actionAddTab(int $id): Response
     {
         $tool = \Craft::$container->get(AddTabToFieldLayout::class);
-        return $this->callTool($tool->add(...), ['fieldLayoutId' => $id]);
+        return $this->callTool($tool(...), ['fieldLayoutId' => $id]);
     }
 
     public function actionAddField(int $id): Response
     {
         $tool = \Craft::$container->get(AddFieldToFieldLayout::class);
-        return $this->callTool($tool->add(...), ['fieldLayoutId' => $id]);
+        return $this->callTool($tool(...), ['fieldLayoutId' => $id]);
     }
 
     public function actionAddUiElement(int $id): Response
     {
         $tool = \Craft::$container->get(AddUiElementToFieldLayout::class);
-        return $this->callTool($tool->add(...), ['fieldLayoutId' => $id]);
+        return $this->callTool($tool(...), ['fieldLayoutId' => $id]);
     }
 
     public function actionRemoveElement(int $id): Response
     {
         $tool = \Craft::$container->get(RemoveElementFromFieldLayout::class);
-        return $this->callTool($tool->remove(...), ['fieldLayoutId' => $id]);
+        return $this->callTool($tool(...), ['fieldLayoutId' => $id]);
     }
 
     public function actionMoveElement(int $id): Response
     {
         $tool = \Craft::$container->get(MoveElementInFieldLayout::class);
-        return $this->callTool($tool->move(...), ['fieldLayoutId' => $id]);
+        return $this->callTool($tool(...), ['fieldLayoutId' => $id]);
     }
 }
