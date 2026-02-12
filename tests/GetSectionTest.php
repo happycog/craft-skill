db91737dde7c23d6ff3e7ea6617934e35339a70b
markhuot <mark@markhuot.com>
2026-02-12 15:09:02 -0500

    fixing craft 5

--- tests/GetSectionTest.php
+++ tests/GetSectionTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Composer\Semver\Semver;
 use craft\elements\Entry;
 use craft\models\Section;
 use happycog\craftmcp\tools\CreateSection;
@@ -9,29 +10,40 @@ use happycog\craftmcp\interfaces\SectionsServiceInterface;
 use function happycog\craftmcp\helpers\service;
 
 beforeEach(function () {
-    // Clean up any existing test sections before each test
-    $sectionsService = service(SectionsServiceInterface::class);
-    $testHandles = ['testGetSection'];
-
-    foreach ($testHandles as $handle) {
-        $section = $sectionsService->getSectionByHandle($handle);
-        if ($section) {
-            $sectionsService->deleteSection($section);
-        }
-    }
-
     // Track created items for cleanup
     $this->createdSectionIds = [];
     $this->createdEntryTypeIds = [];
 
-    // Helper to create section for testing (in Craft 4, this creates entry types automatically)
-    $this->createSection = function (string $name, string $type, array $options = []) {
+    // Helper to create entry type for testing
+    $this->createEntryType = function (string $name, ?string $handle = null) {
+        $createEntryType = Craft::$container->get(CreateEntryType::class);
+
+        $result = $createEntryType->__invoke(
+            name: $name,
+            handle: $handle
+        );
+
+        $this->createdEntryTypeIds[] = $result['entryTypeId'];
+
+        return $result;
+    };
+    $createEntryType = $this->createEntryType;
+
+    $this->createSection = function (string $name, string $type, ?array $entryTypeIds = null, array $options = []) use ($createEntryType) {
+        if ($entryTypeIds === null) {
+            if (Semver::satisfies(Craft::$app->getVersion(), '>=5.0.0')) {
+                $entryTypeIds = [$createEntryType('Default', 'default')['entryTypeId']];
+            } else {
+                $entryTypeIds = null;
+            }
+        }
+
         $createSection = Craft::$container->get(CreateSection::class);
 
         $result = $createSection->__invoke(
             name: $name,
             type: $type,
-            entryTypeIds: null, // Craft 4 creates default entry types automatically
+            entryTypeIds: $entryTypeIds,
             handle: $options['handle'] ?? null,
             enableVersioning: $options['enableVersioning'] ?? true,
             propagationMethod: $options['propagationMethod'] ?? Section::PROPAGATION_METHOD_ALL,
@@ -47,28 +59,9 @@ beforeEach(function () {
     };
 });
 
-afterEach(function () {
-    // Clean up created sections
-    $sectionsService = service(SectionsServiceInterface::class);
-    foreach ($this->createdSectionIds as $sectionId) {
-        $section = $sectionsService->getSectionById($sectionId);
-        if ($section) {
-            $sectionsService->deleteSection($section);
-        }
-    }
-
-    // Clean up created entry types
-    foreach ($this->createdEntryTypeIds ?? [] as $entryTypeId) {
-        $entryType = $sectionsService->getEntryTypeById($entryTypeId);
-        if ($entryType) {
-            $sectionsService->deleteEntryType($entryType);
-        }
-    }
-});
-
 test('retrieves section with entry types and fields', function () {
-    // Create a section (entry types are created automatically in Craft 4)
-    $section = ($this->createSection)('Test Get Section', 'channel', [
+    // Create a section
+    $section = ($this->createSection)('Test Get Section', 'channel', null, [
         'handle' => 'testGetSection'
     ]);
 
@@ -88,8 +81,8 @@ test('retrieves section with entry types and fields', function () {
 });
 
 test('includes fields in entry type data', function () {
-    // Create a section (entry types are created automatically in Craft 4)
-    $section = ($this->createSection)('Test Section Fields', 'channel', [
+    // Create a section
+    $section = ($this->createSection)('Test Section Fields', 'channel', null, [
         'handle' => 'testGetSection'
     ]);
 
@@ -102,8 +95,8 @@ test('includes fields in entry type data', function () {
 });
 
 test('includes section metadata', function () {
-    // Create a section (entry types are created automatically in Craft 4)
-    $section = ($this->createSection)('Test Section Metadata', 'channel', [
+    // Create a section
+    $section = ($this->createSection)('Test Section Metadata', 'channel', null, [
         'handle' => 'testGetSection',
         'enableVersioning' => false,
         'propagationMethod' => 'siteGroup'
@@ -126,9 +119,23 @@ test('throws exception for non-existent section', function () {
 });
 
 test('returns multiple entry types for section', function () {
-    // In Craft 4, sections create a default entry type automatically
-    // We can't easily add multiple entry types via CreateEntryType in Craft 4
-    // since entry types must be associated with sections at creation time
-    // This test is skipped for Craft 4
-    $this->markTestSkipped('Multiple entry types per section requires different approach in Craft 4');
-});
+    // Create multiple entry types
+    $entryType1 = ($this->createEntryType)('Entry Type One', 'entryTypeOne');
+    $entryType2 = ($this->createEntryType)('Entry Type Two', 'entryTypeTwo');
+
+    // Create section with both entry types
+    $section = ($this->createSection)('Multi Entry Type Section', 'channel', [
+        $entryType1['entryTypeId'],
+        $entryType2['entryTypeId']
+    ], [
+        'handle' => 'testGetSection'
+    ]);
+
+    // Get the section
+    $tool = Craft::$container->get(GetSection::class);
+    $result = $tool->__invoke(sectionId: $section['sectionId']);
+
+    expect($result['entryTypes'])->toHaveCount(2)
+        ->and($result['entryTypes'][0]['name'])->toBe('Entry Type One')
+        ->and($result['entryTypes'][1]['name'])->toBe('Entry Type Two');
+})->skip(Semver::satisfies(Craft::$app->getVersion(), '<5.0.0'), 'Craft 5+ supports multiple entry types per section');
